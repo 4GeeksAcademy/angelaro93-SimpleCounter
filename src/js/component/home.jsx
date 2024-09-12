49bed5ac0cd48d5b1ca9f1051cad6da58a7e2b36
angelaro93 <angela.ro.93@gmail.com>
2024-09-12 18:25:49 +0000

    last commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+const [seconds,setSeconds] = useState(0);
+const [decimalSeconds,setDecimalSeconds] = useState(0);
+const [minutes,setMinutes] = useState(0);
+const [decimalMinutes,setDecimalMinutes] = useState(0);
+const [hour,sethour] = useState(0);
+const [decimalhour,setDecimalHour] = useState(0);
+
+useEffect(()=>{
+	setInterval(()=>{
+		setSeconds(prev => prev + 1)
+	},1000)	
+},[])
+
+useEffect(()=>{
+	if(seconds==10) {
+		setDecimalSeconds(prev => prev + 1)
+		setSeconds(0)
+	}
+	if (decimalSeconds==6) {
+		setMinutes(prev => prev + 1)
+		setDecimalSeconds(0)
+	}
+	if (minutes==6) {
+		setDecimalMinutes(prev => prev + 1)
+		setMinutes(0)
+	}
+	if (decimalMinutes==6) {
+		sethour(prev => prev + 1)
+		setDecimalMinutes(0)
+	}
+	if (hour==6) {
+		setDecimalHour(prev => prev + 1)
+		sethour(0)
+	}
+},[seconds]) 
+
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="p-3 mb-2 bg-dark text-white text-center">
+		<div className="text-center d-flex justify-content-center">
+		<div className="p-3 mb-2  bg-dark bg-gradient text-white">
+			<div>
+			<FontAwesomeIcon icon={faClock} />
+			</div>
+			</div> 
+		<div className="p-3 mb-2  bg-dark bg-gradient text-white text-center">
+			<div>
+			{decimalhour}
+			</div> 
+			</div>
+			<div className="p-3 mb-2  bg-dark bg-gradient text-white text-center">    
+			<div>
+				{hour}
+			</div>
+			</div>
+			<div className="p-3 mb-2  bg-dark bg-gradient text-white text-center">
+			<div>
+				{decimalMinutes}
+			</div>
+			</div>
+			<div className="p-3 mb-2  bg-dark bg-gradient text-white text-center">
+			<div>
+				{minutes} 
+			</div>
+			</div>
+            <div className="p-3 mb-2  bg-dark bg-gradient text-white text-center">
+			<div>
+				{decimalSeconds}
+			</div>
+			</div>
+            <div className="p-3 mb-2  bg-dark bg-gradient text-white text-center">
+			<div>
+				{seconds}
+			</div>
+			</div>
+		</div>
 		</div>
 	);
 };
